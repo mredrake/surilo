141c0e5389ae3d94b2ddc75a06d2fd953d5c229b
Dipesh Acharya <xtranophilist@gmail.com>
2017-03-30 12:05:22 +0545

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname + '/static',
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }
+        ],
+        
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
