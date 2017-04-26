6c1fc3cd7cda2e2573b473bf987a8b13e000d4fc
Dipesh Acharya <xtranophilist@gmail.com>
2017-04-26 21:31:37 +0545

    Minor cleanup

--- src/containers/PlayerContainer.js
+++ src/containers/PlayerContainer.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import YouTube from 'react-youtube';
-import Sound from 'react-sound';
 import {Mp3Player} from "../components/Mp3Player";
 
 export class PlayerContainer extends Component {
